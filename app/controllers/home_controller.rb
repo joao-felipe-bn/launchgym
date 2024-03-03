ece2bc0aeeae3ca7b893181234b97d3326a6f0f3
João Felipe <jfbnfelipe.jfbn@gmail.com>
2024-03-03 18:09:38 -0300

    add bootstrap login page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
+
+  before_action :authenticate_user!
+  
   def index
+    @users = User.all
   end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  # attr_accessor :email, :password, :password_confirmation, :current_password, :name
+  # attr_accessor :email, :password, :password_confirmation, :current_password, :name
+
 end
