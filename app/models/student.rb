bcd97b69797c14f4026ea2d163c5079a6d3d29b5
João Felipe <jfbnfelipe.jfbn@gmail.com>
2024-06-04 21:46:29 -0300

    Desing in the page is done

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,7 @@
 class Student < ApplicationRecord
   belongs_to :user
 
-  has_many :class_session_students, dependent: :destroy
+  #has_many :class_session_students, dependent: :destroy
+  has_many :class_session_students
   has_many :class_sessions, through: :class_session_students
 end
